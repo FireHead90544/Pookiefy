a4319d193f626a0565d9896426cc605db44c4a61
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-07 18:42:32 +0530

    feat: update font to roboto

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ["latin"] });
 
 export const metadata = {
   title: "Pookiefy",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
