4f6ad948dc1c45c7c3b8967db8234bb5f06135b1
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-07 19:19:49 +0530

    feat: add authorize component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import PookiefyAuthorize from "@/components/PookiefyAuthorize";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-6xl font-thin">Hello World</h1>
+    <main className="h-screen p-24">
+      <PookiefyAuthorize />
     </main>
   );
 }
