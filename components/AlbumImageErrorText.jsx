e63da3eff076e88417aeec940bd6015217cac34f
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-08 01:35:42 +0530

    feat: create 404 image text component

---
+++ components/AlbumImageErrorText.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PiSmileySadThin } from "react-icons/pi";
+
+const AlbumImageErrorText = () => {
+  return (
+    <PiSmileySadThin className='w-full h-full ' />
+  )
+}
+
+export default AlbumImageErrorText
