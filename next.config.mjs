f266c2fd5608b6dec44155167c59ec2be1ae6546
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-07 22:58:39 +0530

    feat: allow spotify image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i2o.scdn.co",
+				port: "",
+				pathname: "/image/**",
+			},
+		],
+	},
+};
 
 export default nextConfig;
