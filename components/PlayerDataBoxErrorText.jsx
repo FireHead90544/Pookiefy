9c6f8138440f1610a27d9b91b366bf240833e353
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-08 01:48:09 +0530

    feat: create player offline error component

---
+++ components/PlayerDataBoxErrorText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlayerDataBoxErrorText = () => {
+  return (
+    <div className='flex items-center justify-center h-full w-full text-4xl font-light'>
+        Not Playing!
+    </div>
+  )
+}
+
+export default PlayerDataBoxErrorText
