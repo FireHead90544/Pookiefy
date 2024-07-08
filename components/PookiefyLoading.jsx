24ebcad5e75bb57e11be9693646a8bbd2b70f9e7
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-08 22:30:57 +0530

    feat (frontend): create (steal lmao) loading component

---
+++ components/PookiefyLoading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaSpotify } from "react-icons/fa";
+
+const PookiefyNowPlaying = () => {
+	return (
+		<div className="flex flex-col items-center justify-center h-full space-y-8">
+			<div className="flex flex-row space-x-3 text-6xl font-light">
+				<FaSpotify className="text-[#1db954]" />
+				<h1 className="">Pookiefy</h1>
+			</div>
+		</div>
+	);
+};
+
+export default PookiefyNowPlaying;
