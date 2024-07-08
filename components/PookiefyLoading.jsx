f51d2930380a59df71349a64c76545bd0e3442dd
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-08 22:46:00 +0530

    feat: add pulse animation to loading

--- components/PookiefyLoading.jsx
+++ components/PookiefyLoading.jsx
@@ -3,7 +3,7 @@ import { FaSpotify } from "react-icons/fa";
 
 const PookiefyNowPlaying = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-full space-y-8">
+		<div className="flex flex-col items-center justify-center h-full space-y-8 animate-pulse duration-300">
 			<div className="flex flex-row space-x-3 text-6xl font-light">
 				<FaSpotify className="text-[#1db954]" />
 				<h1 className="">Pookiefy</h1>
