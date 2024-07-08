2d784dd74425c6da1555e01c28f20635bfda58fb
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-09 02:59:48 +0530

    feat: hydrate content using api (fix bunch of shit bugs)

--- components/SpotifyPlayerDataBox.jsx
+++ components/SpotifyPlayerDataBox.jsx
@@ -1,41 +1,125 @@
 "use client";
-import React from 'react'
-import Image from 'next/image'
-import SpotifyPlayhead from '@/components/SpotifyPlayhead'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import SpotifyPlayhead from '@/components/SpotifyPlayhead';
 import AlbumImageErrorText from '@/components/AlbumImageErrorText';
 import PlayerDataBoxErrorText from '@/components/PlayerDataBoxErrorText';
 
 const SpotifyPlayerDataBox = () => {
-  const {title, album, artists, progress, duration} = {
-    title: "Why Do I?",
-    album: "Single",
-    artists: ["Bri Tolani", "Unknown Brain"],
-    progress: 14352,
-    duration: 45362
+  const [playerData, setPlayerData] = useState({});
+  const [tokens, setTokens] = useState({ access_token: '', refresh_token: '' });
+
+  const fetchPlayerData = async (access_token, refresh_token) => {
+    try {
+      let resp = await fetch('https://api.spotify.com/v1/me/player', {
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        }
+      });
+
+      if (resp.status === 401) {
+        const refreshResp = await fetch('/api/refresh-access-token', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ refresh_token })
+        });
+
+        const refreshData = await refreshResp.json();
+        access_token = refreshData.access_token;
+
+        localStorage.setItem('access_token', access_token);
+        setTokens({ access_token, refresh_token });
+
+        resp = await fetch('https://api.spotify.com/v1/me/player', {
+          headers: {
+            'Authorization': `Bearer ${access_token}`
+          }
+        });
+      }
+
+      const data = await resp.json();
+
+      setPlayerData({
+        title: data.item.name,
+        url: data.item.external_urls.spotify,
+        imageUrl: data.item.album.images[0].url,
+        album: { name: data.item.album.name, url: data.item.album.external_urls.spotify },
+        artists: data.item.artists.map((artist) => ({
+          name: artist.name,
+          url: artist.external_urls.spotify
+        })),
+        progress: data.progress_ms,
+        duration: data.item.duration_ms
+      });
+
+    } catch (error) {
+      setPlayerData({});
+    }
   }
+
+  useEffect(() => {
+    const access_token = localStorage.getItem('access_token');
+    const refresh_token = localStorage.getItem('refresh_token');
+    setTokens({ access_token, refresh_token });
+
+    const intervalId = setInterval(() => {
+      fetchPlayerData(access_token, refresh_token);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    if (tokens.access_token && tokens.refresh_token) {
+      fetchPlayerData(tokens.access_token, tokens.refresh_token);
+    }
+  }, [tokens]);
+
   return (
-    <div className='flex space-x-4 w-2/3 h-1/2 hover:scale-105 transition-all duration-500 ease-in-out'>
-        <div className='w-1/3 h-full border-2 rounded-sm border-[#191414]'>
-            <Image className="w-full h-full" src={"https://i2o.scdn.co/image/ab67706c0000cfa3bd003bd5a830f78d58228b01"} width={300} height={300}/>
-            {/* <AlbumImageErrorText /> */}
-        </div>
-        <div className='w-2/3 h-full border-2 rounded-sm border-[#191414]'>
-            <div className='flex flex-col justify-center h-full'>
-                <div className='h-2/3 p-8 flex-col items-center'>
-                    <h1 className='text-2xl font-semibold'>{title}</h1>
-                    <div className="flex space-x-2">
-                        <h2 className='text-lg font-light italic'>{album}</h2>
-                        <h2 className='text-lg font-thin italic'>({artists.map((artist) => artist).join(', ')})</h2>
-                    </div>
+    <div className='flex space-x-4 w-2/3 h-1/2 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer'>
+      <div className='w-1/3 h-full border-2 rounded-sm border-[#191414]'>
+        {playerData.imageUrl ? (
+          <Image className="w-full h-full" src={playerData.imageUrl} width={640} height={640} />
+        ) : (
+          <AlbumImageErrorText />
+        )}
+      </div>
+      <div className='w-2/3 h-full border-2 rounded-sm border-[#191414]'>
+        <div className='flex flex-col justify-center h-full'>
+          {Object.keys(playerData).length !== 0 ? (
+            <>
+              <div className='h-2/3 p-8 flex-col items-center'>
+                <Link href={playerData.url}>
+                  <h1 className='text-2xl font-semibold'>{playerData.title}</h1>
+                </Link>
+                <div className="flex space-x-2">
+                  <Link href={playerData.album.url}>
+                    <h2 className='text-lg font-light italic'>{playerData.album.name}</h2>
+                  </Link>
+                  <h2 className='text-lg font-thin italic'>
+                    ({playerData.artists.map((artist, index) => (
+                      <React.Fragment key={artist.url}>
+                        <Link href={artist.url}>{artist.name}</Link>
+                        {index < playerData.artists.length - 1 && ', '}
+                      </React.Fragment>
+                    ))})
+                  </h2>
                 </div>
-                <div className='h-1/3 px-8 py-2'>
-                    <SpotifyPlayhead progress={progress} duration={duration} />
-                </div>
-            </div>
-            {/* <PlayerDataBoxErrorText /> */}
+              </div>
+              <div className='h-1/3 px-8 py-2'>
+                <SpotifyPlayhead progress={playerData.progress} duration={playerData.duration} />
+              </div>
+            </>
+          ) : (
+            <PlayerDataBoxErrorText />
+          )}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SpotifyPlayerDataBox
+export default SpotifyPlayerDataBox;
