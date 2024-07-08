7cf7847db576db71b75350cbe52bfb59e989ba58
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-08 22:29:25 +0530

    chore: update button to Link component

--- components/SpotifyAuthorizeButton.jsx
+++ components/SpotifyAuthorizeButton.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Link from "next/link";
 import { FaSpotify } from "react-icons/fa";
 
 const SpotifyAuthorizeButton = () => {
   return (
-    <button className="flex px-4 py-2 rounded-lg space-x-2 text-lg bg-[#191414] transition-transform duration-300 hover:scale-110">
+    <Link className="flex px-4 py-2 rounded-lg space-x-2 text-lg bg-[#191414] transition-transform duration-300 hover:scale-110" href={'/api/authorize'}>
         <FaSpotify className="my-auto text-[#1db954]" />
         <span className="font-light">Authorize</span>
-    </button>
+    </Link>
   )
 }
 
