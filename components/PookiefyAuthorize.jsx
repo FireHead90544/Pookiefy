9b91a359d0c11a4827adb5c8a1608fd80081e2d7
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-09 06:15:31 +0530

    feat: make authorize components responsive

--- components/PookiefyAuthorize.jsx
+++ components/PookiefyAuthorize.jsx
@@ -3,8 +3,8 @@ import SpotifyAuthorizeButton from '@/components/SpotifyAuthorizeButton';
 
 const PookiefyAuthorize = () => {
   return (
-		<div className="flex flex-col items-center justify-center h-full space-y-4">
-			<h1 className="text-6xl font-light">Pookiefy</h1>
+		<div className="flex flex-col items-center justify-center h-full space-y-4 p-4 md:p-8">
+			<h1 className="text-4xl md:text-6xl font-light">Pookiefy</h1>
 			<SpotifyAuthorizeButton />
 		</div>
 	);
--- components/SpotifyAuthorizeButton.jsx
+++ components/SpotifyAuthorizeButton.jsx
@@ -4,7 +4,7 @@ import { FaSpotify } from "react-icons/fa";
 
 const SpotifyAuthorizeButton = () => {
   return (
-    <Link className="flex px-4 py-2 rounded-lg space-x-2 text-lg bg-[#191414] transition-transform duration-300 hover:scale-110" href={'/api/authorize'}>
+    <Link className="flex items-center px-4 py-2 rounded-lg space-x-2 text-base md:text-lg bg-[#191414] transition-transform duration-300 hover:scale-110" href={'/api/authorize'}>
         <FaSpotify className="my-auto text-[#1db954]" />
         <span className="font-light">Authorize</span>
     </Link>
